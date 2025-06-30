7fe56551d725b0162d80f7e5d09decfaf3b4e917
Manasraj2004 <160581417+Manasraj2004@users.noreply.github.com>
2025-06-30 16:17:47 +0530

    improved navbar

--- app.js
+++ app.js
@@ -1,43 +1,51 @@
 require("dotenv").config();
-const express=require("express")
-const app=express();
-const path= require("path");
-const router =require("./routes/user.js")
+const express = require("express");
+const app = express();
+const path = require("path");
 
-const blogRouter =require("./routes/blog.js")
-const {Blog}=require("./models/blog.js")
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
 
-const mongoose=require("mongoose")
-mongoose.connect(process.env.MONGODB_CONNECT_URL).then(e=>console.log("mongodb connected"))
-
-const PORT=process.env.PORT||8000;
-const cookieParser =require("cookie-parser");
 const { checkForAuthenicationCookie } = require("./middlewares/authentication.js");
-//calling view engine for ejs
-app.set("view engine",'ejs')
-app.set("views",path.resolve("./views"))
+
+const userRoutes = require("./routes/user.js");
+const blogRoutes = require("./routes/blog.js");
+const { Blog } = require("./models/blog.js");
+
+//  Connect MongoDB
+mongoose.connect(process.env.MONGODB_CONNECT_URL)
+  .then(() => console.log(" MongoDB connected"))
+  .catch(err => console.error(" MongoDB connection error:", err));
+
+//  Set up view engine
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+// Middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.resolve("./public")));
 app.use(cookieParser());
-app.use(checkForAuthenicationCookie("token"))
-app.use(express.urlencoded({extended:true}));
-app.use(express.static(path.resolve("./public")))
+app.use(checkForAuthenicationCookie("token"));
+
+app.use((req, res, next) => {
+  res.locals.user = req.user;
+  next();
+});
 
 
-
-app.use("/user",router)
-app.use("/blog",blogRouter)
-
-
-//allowing express to serve files in public folder
-
-
-app.get("/",async(req,res)=>{
-    const allBlogs=await Blog.find({});
-    return res.render("home",{
-    user:req.user,
-    blogs:allBlogs,
-    });
+app.use("/user", userRoutes);
+app.use("/blog", blogRoutes);
+//global access
+app.get("/", async (req, res) => {
+  const allBlogs = await Blog.find({}).populate("createdBy");
+  res.render("home", {
+    blogs: allBlogs
     
-})
-app.listen(PORT,()=>{
-    console.log(`Server started at PORT:${PORT}`)
-})
+  });
+});
+
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(` Server running on PORT: ${PORT}`);
+});
